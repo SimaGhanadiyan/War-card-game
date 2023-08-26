608dfb4def799d05e1459552d74eee6677c4394c
Sima Gh <sima.z.ghanadiyan@gmail.com>
2023-08-26 19:17:25 +0430

    logic part

--- main.py
+++ main.py
@@ -53,3 +53,72 @@ class Player:
     
     def __str__(self):
         return f'player {self.name} has {len(self.all_cards)} cards'
+    
+player_one = Player("One")
+player_two = Player("Two")
+
+new_deck = Deck()
+new_deck.shuffle()
+
+for x in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+
+round_num = 0
+game_on = True
+
+while game_on:
+    round_num += 1
+    print(f"Round {round_num}")
+    
+    if len(player_one.all_cards) == 0:
+        print('Player One , out of cards! Player Two wins!')
+        game_on = False
+        break
+    
+    if len(player_two.all_cards) == 0:
+        print('Player Two , out of cards! Player One wins!')
+        game_on = False
+        break
+    
+    # Start A New Round
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+    
+    player_two_cards = []
+    player_two_cards.append(player_two.remove_one())
+    
+    
+    at_war = True 
+    
+    while at_war:
+        
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+            player_one.add_cards(player_one_cards)
+            player_one.add_cards(player_two_cards)
+            at_war = False
+        elif player_one_cards[-1].value < player_two_cards[-1].value:
+            player_two.add_cards(player_one_cards)
+            player_two.add_cards(player_two_cards)
+            at_war = False
+        else:
+            print('WAR !')
+            
+        
+        if len(player_one.all_cards) < 3 :
+            print('player One unable to declare war')
+            print('player Two WINS !')
+            game_on = False
+            break
+        
+        elif len(player_one.all_cards) < 3 :
+            print('player One unable to declare war')
+            print('player Two WINS !')
+            game_on = False
+            break
+        
+        else:
+            for num in range(3):
+                player_one_cards.append(player_one.remove_one())
+                player_two_cards.append(player_two.remove_one())
+
